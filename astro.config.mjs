93200ab16e45ad88755d42b931e8613f991f0e8b
Sam <sam@Sams-Mac-mini.local>
2026-02-05 11:49:54 -0500

    Fix Tailwind configuration and CSS syntax error in Layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zacharyhutz-sudo.github.io',
-  base: 'Sherri-Beach-Home',
-  integrations: [tailwind()]
+  site: "https://zacharyhutz-sudo.github.io",
+  base: "Sherri-Beach-Home",
+  integrations: [tailwind()],
 });
