8d439ba8326d8f06f882e7c910d8426ac5c46618
Sam <sam@Sams-Mac-mini.local>
2026-02-09 14:37:56 -0500

    update site url and base for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://zacharyhutz-sudo.github.io",
-  base: "/Sherri-Beach-Home",
+  site: "https://theresalwaystimeforthesea.com",
+  base: "/",
   integrations: [tailwind()],
 });
